e3d853b33e6d4241b5dda589dead900c4e1eac88
Jamieatucker <jamieatucker4@gmail.com>
2020-06-24 23:14:31 +0000

    Target files to showcase site on preview.

--- capstone/target/capstone-1/script.js
+++ capstone/target/capstone-1/script.js
@@ -19,32 +19,6 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 /**
- * Adds a random fact to the home page.
- */
-function addRandomFact() {
-  // Some hard-coded facts about me
-  const facts = [
-      'I was born in Orlando, Florida!',
-      'I have moved three times in my life, from Florida, to Virginia, to Michigan.',
-      'I have an older brother who recently graduated from Florida A&M University!',
-      'I learned Japanese for two years in high school (and still know how to speak some phrases)!',
-      'I won an Xbox One S at a local NBA 2K Video Game Tournament in 2017!',
-      'I am a Detroit Lions fan.',
-      'I know how to create YouTube Videos using Adobe After Effects.',
-      'I played basketball for 10 years, and my favorite team is the Portland Trailblazers.'
-   ];
-
-  // Pick a random fact
-  let fact = facts[Math.floor(Math.random() * facts.length)];
-
-  // Add it to the page
-  const factContainer = document.querySelector('#fact-container')
-  factContainer.style.visibility = 'visible';
-  factContainer.style.display = 'block';
-  factContainer.innerText = fact;
-}
-
-/**
  * Deletes all the comments from the 'Comments' servlet.
  */
 async function deleteDataUsingAsyncAwait() {
@@ -112,67 +86,6 @@ async function getDataUsingAsyncAwait() {
 }
 
 /**
- * Get all the teams in the NBA.
- */
-async function getNbaTeamsUsingAsyncAwait() {
-  const response = await fetch("https://free-nba.p.rapidapi.com/teams?page=0", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-nba.p.rapidapi.com",
-		"x-rapidapi-key": 
-            "20d91d87d5mshfd44d974ad4d8ccp15d9cfjsn12bdf5fc1d42"
-	}
-  });
-  const teams = await response.json();
-
-  var text = "";
-  for(i = 0; i < teams.data.length; i++){
-    text += "<b>" + teams.data[i].city + " " + teams.data[i].name + "</b><br>";
-  }
-
-  // Add the teams to the page
-  const nba = document.querySelector('#nba-data');
-  nba.style.visibility = 'visible';
-  nba.style.display = 'block';
-  nba.innerHTML = text;
-}
-
-/**
- * Generate a random player that plays in the NBA.
- */
-async function getRandomNbaPlayerUsingAsyncAwait() {
-  // There are 494 current NBA players on a team
-  var rand = Math.floor((Math.random() * 494));
-  const response = await fetch("https://free-nba.p.rapidapi.com/players/" + rand + "\"", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-nba.p.rapidapi.com",
-		"x-rapidapi-key": "20d91d87d5mshfd44d974ad4d8ccp15d9cfjsn12bdf5fc1d42"
-	}
-  });
-  const player = await response.json();
-
-  // Formatting the player's details on to the page
-  var text = "<b>" + player.team.full_name + "</b><br><i>" + player.first_name + " " + player.last_name + " " + 
-      player.height_feet + "\'" + player.height_inches + 
-      "\" " + player.position + "</i>";
-
-  // Add the random player to the page
-  const nbaPlayer = document.querySelector('#nba-player');
-  nbaPlayer.style.visibility = 'visible';
-  nbaPlayer.style.display = 'block';
-  nbaPlayer.innerHTML = text;
-}
-
-/**
- * Hides the comment section (only works if logged in).
- */
-function hideData() {
-  document.querySelector('#data-container').style.visibility = 'hidden';
-  document.querySelector('#data-container').style.display = 'none';
-}
-
-/**
  * Manages the visibility of certain content based on the login status of the user.
  */
 function manageVisibility() {
@@ -208,24 +121,6 @@ function manageVisibility() {
     unauth.style.display = 'none';
     });
   }
-
-/**
- * Reveals my hidden talent to the hidden talent page.
- */
-function revealHiddenTalent() { 
-  // Add the hidden talent on the page
-  const hiddenTalentContainer = document.querySelector('#hiddentalent-container');
-  hiddenTalentContainer.style.visibility = 'visible';
-  hiddenTalentContainer.style.display = 'block';
-
-  // Reveal the GIF on the page
-  document.querySelector('#rollsafe').style.visibility = 'visible';
-  document.querySelector('#rollsafe').style.display = 'block';
-
-  // Reveal the audio file on the page
-  document.querySelector('#music').style.visibility = 'visible';
-  document.querySelector('#music').style.display = 'inline';
-}
 
 /**
  * Submits the comment to the '/comment' servlet.
